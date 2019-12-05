131c3d168174cf07b78b4cf61955023602dec2e2
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-05 09:37:20 +0100

    correction

--- index.php
+++ index.php
@@ -3,15 +3,13 @@ $months = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>PHP partie3 exo3</title>
-  </head>
-  <body>
-    <?php
-      foreach ($months as $element) {
-        echo $element . '<br />';
-      };
-    ?>
-  </body>
+<head>
+  <meta charset="utf-8" />
+  <title>PHP partie3 exo3</title>
+</head>
+<body>
+  <?php foreach ($months as $element) { ?>
+    <p><?php echo $element; ?></p>
+  <?php }; ?>
+</body>
 </html>
